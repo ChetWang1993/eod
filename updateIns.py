4ced81d5ea1f7fb1d9ef5b45585ed12ccb8eda59
ChetWang1993 <elwinwang1993@gmail.com>
2019-01-12 16:23:36 +0800

    update ins

---
+++ updateIns.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python
+# encoding: UTF-8
+
+import json
+import sys
+import pandas as pd
+sys.path.append('../trading')
+from func import *
+from datetime import datetime, date, time, timedelta
+
+#root_dir = '/Users/apple/Documents/trading/'
+root_dir = '/root/'
+strat = 'dy'
+ts_format = '%y%m%d'
+conf_path = root_dir + 'trading/conf/OKEX_{}.json'
+ins_path = root_dir + 'eod/data/instrument/{}.txt'
+
+setting = json.load(open(conf_path.format(strat)))
+
+if len(sys.argv) == 2:
+	dt = sys.argv[1]
+else:
+	dt = datetime.now().strftime('%y%m%d')
+
+okApi = okApi(setting['apiKey'], setting['secretKey'],
+	setting['order_log_dir'].format(setting['symbol']) + setting['order_log_name'].format(datetime.now()))
+
+try:
+	ret = okApi.get_okex('/api/futures/v3/instruments')
+
+	ins_list = {}
+
+	for x in ret:
+		if (not x['underlying_index'] in ins_list.keys()) or datetime.strptime(x['instrument_id'].split('-')[2], 
+			ts_format) > datetime.strptime(ins_list[x['underlying_index']].split('-')[2], ts_format):
+			ins_list[x['underlying_index']] = x['instrument_id']
+
+	f = open(ins_path.format(dt), 'w')
+	f.writelines([x + '\n' for x in ins_list.values()])
+
+except Exception as e:
+	print('[ERROR]: fail to generate instrument log for strat {} on {} because {}'.format(strat, dt, e))
+print('[INFO]: generate instrument log for strat {} on {} successfully'.format(strat, dt))
--- updateOrder.py
+++ updateOrder.py
@@ -1,5 +1,9 @@
-sys.path.append('../trading')
+#!/usr/local/bin/python
+# encoding: UTF-8
 import json
+import sys
+import pandas as pd
+sys.path.append('../trading')
 from func import *
 from datetime import datetime, date, time, timedelta
 
@@ -7,30 +11,45 @@ if len(sys.argv) < 3:
 	print('usage: {} symbol strat [dt]'.format(sys.argv[0]))
 	quit()
 
-order_dir = '/root/data/order/{}'
-eod_dir = '/root/data/eod/{}'
-td = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).strftime("%Y-%m-%dT%H:%M:%S`Z")
-conf_path = '/Users/apple/Documents/conf/OKEX_{}.json'
+#root_dir = '/Users/apple/Documents/trading/'
+root_dir = '/root/'
+order_path = root_dir + 'trading/order/{}/{}.txt'
+eod_path = root_dir + 'eod/data/instrument/{}.txt'
+ts_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+conf_path = root_dir + 'trading/conf/OKEX_{}.json'
+symbol = sys.argv[1]
+strat = sys.argv[2]
 
-if len(sys.argv) == 3:
+if len(sys.argv) == 4:
 	dt = sys.argv[3]
 else:
 	dt = datetime.now().strftime('%Y%m%d')
-setting = json.load(conf_path.format(strat))
-symbol = sys.argv[1]
-okApi = okApi(okApi(setting['apiKey'], setting['secretKey'], 
-	setting['order_log_dir'].format(setting['symbol']) + setting['order_log_name'].format(datetime.now()))
-[instrument] = [x for x in list(open(eod_dir.format(dt)).readlines()) if symbol in x]
+yd = (datetime.strptime(dt, '%Y%m%d') - timedelta(1)).replace(hour=0, minute=0, second=0, microsecond=0)
+td = datetime.strptime(dt, '%Y%m%d').replace(hour=0, minute=0, second=0, microsecond=0)
 
-params = {'status': 7, 'instrument_id': instrument}
-ret = okApi.get_okex('/api/futures/v3/orders/{}'.foramt(instrument), params)
-ks = ['instrument_id', 'size', 'timestamp', 'filled_qty', 'fee', 'order_id', 'price', 'price_avg',
-	'status', 'type', 'contract_val', 'leverage']
-orders = [dict(zip(ks, [x['instrument_id'], x['size'], 
-	datetime.strptime(x['timestamp'], '%Y-%m-%dT%H:%M:%S.%f').replace(tzinfo=timezone('GMT')).astimezone(timezone('Asia/Singapore')),
-	x['filled_qty'], x['fee'], x['order_id'], x['price'], x['price_avg'], x['status'], 
-	x['type'], x['contract_val'], x['leverage']])) 
-	for x in ret['order_info']]
-orders = [x for x in x if datetime.strptime(x['timestamp'], '%Y-%m-%dT%H:%M:%S.%f') <= td]
-orders_yd = pd.DataFrame(orders)
-print(orders_yd)
+setting = json.load(open(conf_path.format(strat)))
+
+okApi = okApi(setting['apiKey'], setting['secretKey'],
+	setting['order_log_dir'].format(setting['symbol']) + setting['order_log_name'].format(datetime.now()))
+[instrument] = [x.rstrip() for x in list(open(eod_path.format(dt)).readlines()) if symbol in x]
+
+try:
+	ret = okApi.get_okex('/api/futures/v3/orders/{}?status=7'.format(instrument))
+
+	ks = ['instrument_id', 'size', 'timestamp', 'filled_qty', 'fee', 'order_id', 'price', 'price_avg',
+		'status', 'type', 'contract_val', 'leverage']
+	orders = [dict(zip(ks, [x['instrument_id'], x['size'],
+		datetime.strptime(x['timestamp'],
+			ts_format).replace(tzinfo=timezone('GMT')).astimezone(timezone('Asia/Singapore')).strftime(ts_format),
+		x['filled_qty'], x['fee'], x['order_id'], x['price'], x['price_avg'], x['status'], 
+		x['type'], x['contract_val'], x['leverage']])) 
+		for x in ret['order_info']]
+
+	orders = [x for x in orders if datetime.strptime(x['timestamp'], ts_format) <= td and
+		datetime.strptime(x['timestamp'], ts_format) >= yd]
+	orders_yd = pd.DataFrame(orders)
+
+	orders_yd.to_csv(order_path.format(strat, dt), index = False, sep = '\t')
+except Exception as e:
+	print('[ERROR]: fail to generate order log for strat {} on {} because {}'.format(strat, dt, e))
+print('[INFO]: generate order log for strat {} on {} successfully'.format(strat, dt))
